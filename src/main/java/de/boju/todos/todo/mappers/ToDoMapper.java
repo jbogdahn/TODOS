497f4f1b249642471634cb1b097d56617100fd60
jbogdahn <jbogdahn@in-technology.eu>
2024-06-24 17:30:07 +0200

    remove unused code

--- src/main/java/de/boju/todos/todo/mappers/ToDoMapper.java
+++ src/main/java/de/boju/todos/todo/mappers/ToDoMapper.java
@@ -5,7 +5,6 @@ import de.boju.todos.model.ToDoDto;
 import de.boju.todos.todo.entities.ToDoEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper
 public interface ToDoMapper {
@@ -13,8 +12,7 @@ public interface ToDoMapper {
     ToDoDto toDto(ToDoEntity entity);
 
     @Mapping(target = "version", ignore = true)
-    ToDoEntity toEntity(ToDoDto dto);
-
-    @Mappings({@Mapping(target = "version", ignore = true), @Mapping(target = "done", constant = "false"), @Mapping(target = "id", ignore = true)})
+    @Mapping(target = "done", constant = "false")
+    @Mapping(target = "id", ignore = true)
     ToDoEntity toEntity(NewToDoDto dto);
 }
