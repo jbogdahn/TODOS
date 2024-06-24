c7a44996124fd490b5b68e2afce18f2b5ed9f33e
jbogdahn <jbogdahn@in-technology.eu>
2024-06-24 13:57:48 +0200

    redo frontend

--- todos-webapp/src/app/app.component.ts
+++ todos-webapp/src/app/app.component.ts
@@ -1,18 +1,13 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {TodolistComponent} from "./todolist/todolist.component";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodolistComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todos-webapp';
-
-  getAllToDos(): void {
-    location.reload();
-  }
 }
